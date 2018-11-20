bbb6ddf5b0542fe5c688bebdf84e528373900ab4
Ciara Southgate <ciarajsouthgate@gmail.com>
2018-11-20 12:08:26 -0800

    added some comments

--- js/home.js
+++ js/home.js
@@ -126,12 +126,18 @@ function createMedDiv(name, genOrBrand, genName, str, un, freq, time, times, aux
   
 }
 
+//Sorts existing time array
 function Comparator(a, b) {
   if (a[0] < b[0]) return -1;
-  if (a[0] > b[0]) return 1;
-  return 0;
+  else if (a[0] > b[0]) return 1;
+  else {
+    if (a[1] < b[1]) return -1;
+    if (a[1] > b[1]) return 1;
+    return 0;
+  }
 }
 
+//Creates divs for all times that need meds
 function createTimeDivs() {
   existingTimes.sort(Comparator);
   console.log(existingTimes);
