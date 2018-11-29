93f5fc99fa24f0dea75c81abf85c2a9d64c9a03c
Ciara Southgate <ciarajsouthgate@gmail.com>
2018-11-28 17:41:17 -0800

    moved greeting into main js file

--- public/js/home.js
+++ public/js/home.js
@@ -595,3 +595,20 @@ function populatePharmacy() {
   $("#pharmMessage").css("display", "none");
   $("#pharmEdit").css("display", "inline-block");
 }
+
+function greeting() {
+  var firebase = app_firebase;
+  var welcome = document.getElementById("welcome");
+  var userName = "";
+
+  firebase.auth().onAuthStateChanged(function(user){
+    if (user){
+      userName=user.displayName;
+    } else {
+      userName="test";
+    }
+        welcome.innerText = "Welcome, " + userName;
+    });
+ };
+
+greeting();
